209a8aa2b6ccc1b47303acd85951ac4616f57491
Victor Zhang <victorzhang6897@gmail.com>
2015-10-29 14:45:12 +1100

    starting search parsing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,12 @@
 class SearchController < ApplicationController
   def search
+    @special = ["(",")","@","#","^"]
+    @stopwords = ["(",")","@","#","^"]
+    route = File.join(Rails.root, 'app', 'assets', 'other', 'stopwords');
+    @stopwords = File.read(route)
+    @arr = Array.new()
+    
+
+    @test = params[:q];
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,5 @@ class WelcomeController < ApplicationController
   end
 
   def show_results
-
   end
 end
