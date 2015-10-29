7e5c12f26e735ddf5c0a1d62012385ab05d54f23
Ben Soh <neb_hos@Hotmail.com>
2015-10-29 14:34:31 +1100

    changed front page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,11 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+
+$(document.ready(function () {
+  $("#search-button").click(function () {
+    $("#everything").animate({
+      'marginLeft' : "+=30px"
+    });
+  });
+});
