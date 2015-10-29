6679e72c670328527c9bfdbddc29401800d44955
Ben Soh <neb_hos@Hotmail.com>
2015-10-29 18:15:35 +1100

    added results

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,7 +1,7 @@
 module WelcomeHelper
    def result(skill)
-      string = ["<div>",
-         
+      string = ["<div class='individual-result'>",
+         "#{skill}",
          "</div>"].join("\n") + "\n"
    end
 end
